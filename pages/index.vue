c791c0d2483f45c1609522e229512ce82c0f22e3
Masaki Fujie <fujiemasaki@fujiemasakinoMacBook-Air.local>
2022-02-20 08:36:43 +0900

    パラメーターの取得

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="container">
+    <p>{{message}}</p>
+    <router-link to="/price">pricepage</router-link>
+  </section>
+</template>
+
+<script>
+export default {
+ data: function(){
+    return {
+    message: "Helloworld"
+  }
+ }
+}
+</script>
---
+++ pages/users/_id.vue
@@ -0,0 +1,18 @@
+<template>
+  <section>
+    <h1>{{message}}</h1>
+    <p>id: {{ this.$route.params.id }}</p>
+    <!-- urlパラメーターとして取得するにはこのように記述 -->
+  </section>
+</template>
+
+<script>
+export default {
+  data: function(){
+    return {
+      message: '/users/_id.vueを表示'
+    }
+  }
+}
+</script>
+
