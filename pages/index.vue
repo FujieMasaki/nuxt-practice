ff222a3671acd2a5597f685ebf2dd78c8b78a21f
Masaki Fujie <fujiemasaki@fujiemasakinoMacBook-Air.local>
2022-02-20 17:31:35 +0900

    エラーのした際の表示

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,12 @@
 <template>
   <section class="container">
     <div>
-      {{users[0].id}},{{ users[0].name }}
+      <!-- {{users[0].id}},{{ users[0].name }} -->
+      <ul>
+        <li v-for="user in users" :key="user.id">
+          {{ user.id }},{{ user.name }},{{ user.company.name }}
+        </li>
+      </ul>
     </div>
   </section>
 </template>
@@ -9,16 +14,22 @@
 <script>
 const axios = require('axios')
 // axiosのインスタンス作成
-let url = 'https://jsonplaceholder.typicode.com/users'
+let url = 'https://jsonplaceholder.typicode.com/usersdd'
 
 export default {
-  asyncData({ params }){
+  asyncData({ params, error }){
     return axios.get(url)
     // APIのデータ取得
     .then((res) => {
     // resにはサーバーからのレスポンスデータが入っている
       return {users:res.data}
     })
+    .catch((e => {
+      // console.log(e.response.status)
+      error({ users: e.response.status, messanger: 'Error!!' })
+    }))
+
+
   }
 }
 </script>
